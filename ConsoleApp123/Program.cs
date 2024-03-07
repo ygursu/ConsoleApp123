a3687acf3668b60feb6d2ea697c28eaa316f492c
ygursu <yasemingrs@gmail.com>
2024-03-07 22:07:16 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp123/Program.cs
@@ -0,0 +1,18 @@
+﻿int kontrol = 0;
+Console.Write("Sayı Girin : ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i < sayi)
+{
+    if (sayi % i == 0)
+        kontrol++;
+
+    i++;
+}
+
+if (kontrol != 0)
+    Console.WriteLine("Girdiğiniz sayı asal değildir.");
+else
+    Console.WriteLine("Girdiğiniz sayı asaldır.");
+
+Console.ReadKey();
